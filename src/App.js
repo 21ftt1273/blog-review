74f661327a84656e6eefbb5362d39a7dc18e1684
Amirul Aiman <21FTT1273@student.pb.edu.bn>
2024-01-05 15:38:06 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import blogimage from "./illustration-article.svg";
+import avatarimage from "./image-avatar.webp";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" flex flex-col h-screen items-center justify-center px-7">
+      <div className=" text-center sm:text-left bg-white rounded-[30px] shadow-sm overflow-hidden ">
+        <div>
+          <img className=" rounded-[30px] p-5 " src={blogimage}>
+            <h1>Learning</h1>
+            <h1>Published 21 December 2023</h1>
+            <h1>HTML & CSS FOUNDATION</h1>
+            <h1>
+              These languages are the backbone of every website, 
+              defining structure, content, and presentation
+              </h1>
+              <img className="" src={avatarimage}>
+        </div>
+      </div>
+</div>
     </div>
   );
+    
 }
 
 export default App;
