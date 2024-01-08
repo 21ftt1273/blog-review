adf8e78b46cb79a41605d25784e018fae6e6bf78
Amirul Aiman <21FTT1273@student.pb.edu.bn>
2024-01-08 11:14:48 +0800

    hi

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import "./App.css";
 function App() {
   return (
     <div className=" flex flex-col h-screen items-center justify-center bg-custom-yellow px-7">
-      <div className=" sm:text-left bg-white rounded-[30px] border shadow-sm overflow-hidden ">
+      <div
+        className=" sm:w-96 sm:text-left bg-white rounded-[25px] border-[1px] border-black shadow-sm overflow-hidden "
+        style={{ boxShadow: "0.5rem 0.5rem black" }}
+      >
         <div>
-          <img className=" rounded-[30px] p-5 " src={blogimage} />
-          <div className=" text-left px-5 space-y-5">
-            <h1>Learning</h1>
-            <h1>Published 21 December 2023</h1>
-            <h1>HTML & CSS FOUNDATION</h1>
-            <h1>
+          <img
+            className=" rounded-[30px] p-5 w-full"
+            src={blogimage}
+            alt="blogimage"
+          />
+          <div className=" font-figtree text-left px-5 space-y-5">
+            <span className=" bg-yellow-300 pl-4 pr-4 pt-1 pb-1 font-bold rounded-[6px] text-sm ">
+              Learning
+            </span>
+            <h1 className=" text-[13px] font-medium ">Published 21 Dec 2023</h1>
+            <h1 className=" font-bold text-xl">HTML & CSS Foundations</h1>
+            <h1 className=" text-gray-400 text-[16px]">
               These languages are the backbone of every website, defining
-              structure, content, and presentation
+              structure, content, and presentation.
             </h1>
-            <div className=" flex flex-row items-center pb-5">
-              <img className="" src={avatarimage} />
-              <h1 className=" px-2">Greg Hooper</h1>
+            <div className=" flex flex-row items-center pb-5 ">
+              <img className=" size-8" src={avatarimage} alt="men" />
+              <h1 className=" px-2 font-extrabold text-sm ">Greg Hooper</h1>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"],
+      },
       colors: {
         "custom-yellow": "#F5D04E",
       },
